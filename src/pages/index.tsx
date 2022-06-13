fb40a3bf741d022037623ee41d6d843cd62881e7
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-06-13 12:02:29 +0100

    Change fetch by axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,37 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+
+type Episodes = {
+  id: string
+  title: string
+  members: string
+  published_at: string
+  thumbnail: string
+  description: string
+  file: {
+    url: string
+    type: string
+    duration: number
+  }
+}
+type HomeProps = {
+  episodes: Episodes[]
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>{JSON.stringify(props.episodes)}</div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
   return {
     props: {
       episodes: data
