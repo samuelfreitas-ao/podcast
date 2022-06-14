61a70d3884d318a8c1cd4e50511ddbe0edf58c37
Samuel Freitas <samuelfreitas.ao@gmail.com>
2022-06-14 13:15:11 +0100

    Add home page link on logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import format from 'date-fns/format'
 import pt from 'date-fns/locale/pt-BR'
+import Link from 'next/link'
 
 import styles from './styles.module.scss'
-export function Header(){
+export function Header() {
     const currentData = format(new Date(), 'EEEEEE, d MMMM', {
-        locale:pt
+        locale: pt
     })
     return (
         <header className={styles.headerContainer}>
-            <img src="/logo.svg" alt="Podcast" />
+            <Link href={'/'}>
+                <a><img src="/logo.svg" alt="Podcast" /></a>
+            </Link>
             <p>O melhor para você ouvir, sempre</p>
             <span>{currentData}</span>
         </header>
